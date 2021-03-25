b6008cd8cd17de8a9eba5c5483f47fc74ba743b2
Hashim Shafiq <hashim@yallaparking.com>
2021-03-25 18:23:36 +0500

    converted Signup activity to use view binding

--- app/src/main/java/com/hashim/instagram/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/hashim/instagram/ui/signup/SignupActivity.kt
@@ -6,17 +6,20 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.lifecycle.Observer
-import com.hashim.instagram.R
+import com.hashim.instagram.databinding.ActivitySignupBinding
 import com.hashim.instagram.di.component.ActivityComponent
 import com.hashim.instagram.ui.base.BaseActivity
 import com.hashim.instagram.ui.main.MainActivity
-import com.hashim.instagram.utils.common.Event
 import com.hashim.instagram.utils.common.Status
-import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : BaseActivity<SignupViewModel>(){
 
-    override fun provideLayoutId(): Int = R.layout.activity_signup
+    lateinit var binding: ActivitySignupBinding
+
+    override fun provideLayoutId(): View {
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
     override fun injectDependencies(activityComponent: ActivityComponent) {
         activityComponent.inject(this)
@@ -24,7 +27,7 @@ class SignupActivity : BaseActivity<SignupViewModel>(){
 
     override fun setupView(savedInstanceState: Bundle?) {
 
-        et_password.addTextChangedListener(object : TextWatcher {
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
             }
@@ -38,7 +41,7 @@ class SignupActivity : BaseActivity<SignupViewModel>(){
             }
         })
 
-        et_email.addTextChangedListener(object : TextWatcher {
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
             }
@@ -52,7 +55,7 @@ class SignupActivity : BaseActivity<SignupViewModel>(){
             }
         })
 
-        et_name.addTextChangedListener(object : TextWatcher {
+        binding.etName.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
             }
@@ -66,7 +69,7 @@ class SignupActivity : BaseActivity<SignupViewModel>(){
             }
         })
 
-        bt_login.setOnClickListener {
+        binding.btLogin.setOnClickListener {
             viewModel.doSignup()
         }
     }
@@ -74,53 +77,53 @@ class SignupActivity : BaseActivity<SignupViewModel>(){
     override fun setupObservers() {
         super.setupObservers()
 
-        viewModel.fullNameField.observe(this, Observer {
-            if(et_name.text.toString() != it){
+        viewModel.fullNameField.observe(this, {
+            if(binding.etName.text.toString() != it){
                 viewModel.fullNameField.postValue(it)
             }
         })
 
-        viewModel.nameValidation.observe(this, Observer {
+        viewModel.nameValidation.observe(this, {
             when(it.status) {
-                Status.ERROR -> layout_name.error = it.data?.run { getString(it.data) }
-                else -> layout_name.isErrorEnabled = false
+                Status.ERROR -> binding.layoutName.error = it.data?.run { getString(it.data) }
+                else -> binding.layoutName.isErrorEnabled = false
             }
         })
 
-        viewModel.emailField.observe(this, Observer {
-            if(et_email.text.toString() != it){
+        viewModel.emailField.observe(this, {
+            if(binding.etEmail.text.toString() != it){
                 viewModel.emailField.postValue(it)
             }
         })
 
-        viewModel.emailValidation.observe(this, Observer {
+        viewModel.emailValidation.observe(this, {
             when(it.status) {
-                Status.ERROR -> layout_email.error = it.data?.run { getString(it.data) }
-                else -> layout_email.isErrorEnabled = false
+                Status.ERROR -> binding.layoutEmail.error = it.data?.run { getString(it.data) }
+                else -> binding.layoutEmail.isErrorEnabled = false
             }
         })
 
-        viewModel.passwordField.observe(this, Observer {
-            if(et_password.text.toString() != it){
+        viewModel.passwordField.observe(this, {
+            if(binding.etPassword.text.toString() != it){
                 viewModel.passwordField.postValue(it)
             }
         })
 
-        viewModel.passwordValidation.observe(this, Observer {
+        viewModel.passwordValidation.observe(this, {
             when(it.status) {
-                Status.ERROR -> layout_password.error = it.data?.run { getString(it.data) }
-                else -> layout_password.isErrorEnabled = false
+                Status.ERROR -> binding.layoutPassword.error = it.data?.run { getString(it.data) }
+                else -> binding.layoutPassword.isErrorEnabled = false
             }
         })
 
-        viewModel.launchMain.observe(this, Observer<Event<Map<String,String>>> {
+        viewModel.launchMain.observe(this, {
             it.getIfNotHandled()?.run {
                 startActivity(Intent(applicationContext,MainActivity::class.java))
             }
         })
 
         viewModel.siginningUp.observe(this, Observer {
-            pb_loading.visibility = if(it) View.VISIBLE else View.GONE
+            binding.pbLoading.visibility = if(it) View.VISIBLE else View.GONE
         })
 
     }
